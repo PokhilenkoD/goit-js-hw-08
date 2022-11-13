6eccdb774a4ff2a655095bf7b3de326a669a0bf4
PokhilenkoD <pokhilenko.d@gmail.com>
2022-11-13 20:34:22 +0100

    adds adds solved first and second task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
-console.log('Test');
+const galleryParentEl = document.querySelector('.gallery');
+
+const murkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div>
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+      </div>`
+  )
+  .join('');
+
+galleryParentEl.insertAdjacentHTML('beforeend', murkup);
+
+galleryParentEl.addEventListener('click', onClickGalleryElements);
+
+function onClickGalleryElements(event) {
+  event.preventDefault();
+}
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
