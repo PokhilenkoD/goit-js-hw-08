5c4025cfa6d734ddd55ec6bc176a4eed91788d77
PokhilenkoD <pokhilenko.d@gmail.com>
2022-11-14 17:08:33 +0100

    adds adds solved third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+
+const inputEl = document.querySelector('.feedback-form');
+let localEl ={};
+
+inputEl.addEventListener('input', throttle(onInputForm, 500));
+
+
+function onInputForm(event) {
+    event.preventDefault();
+     localEl = {email: inputEl[0].value,
+        message: inputEl[1].value };
+    
+    localStorage.setItem('feedback-form-state', JSON.stringify(localEl))
+}
+
+
+
+if(localStorage.getItem('feedback-form-state')){
+   const localSaveEl = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+
+    inputEl[0].value = localSaveEl.email;
+    inputEl[1].value = localSaveEl.message;
+
+};
+
+inputEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm (event) {
+    event.preventDefault();
+    localStorage.clear();
+    console.log(localEl);
+    inputEl.reset();
+}
