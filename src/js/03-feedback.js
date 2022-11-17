b61de6b4985ec900f505d18cbb3c8004967ee466
pokhilenkod <pokhilenko.d@gmail.com>
2022-11-17 19:26:51 +0200

    Work on mistakes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,29 @@
 import throttle from 'lodash.throttle';
 
-
 const inputEl = document.querySelector('.feedback-form');
-let localEl ={};
+let localEl = {};
 
 inputEl.addEventListener('input', throttle(onInputForm, 500));
 
-
 function onInputForm(event) {
-    event.preventDefault();
-     localEl = {email: inputEl[0].value,
-        message: inputEl[1].value };
-    
-    localStorage.setItem('feedback-form-state', JSON.stringify(localEl))
+  localEl = { email: inputEl[0].value, message: inputEl[1].value };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(localEl));
 }
 
+if (localStorage.getItem('feedback-form-state')) {
+  const localSaveEl =
+    JSON.parse(localStorage.getItem('feedback-form-state')) || {};
 
-
-if(localStorage.getItem('feedback-form-state')){
-   const localSaveEl = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
-
-    inputEl[0].value = localSaveEl.email;
-    inputEl[1].value = localSaveEl.message;
-
-};
+  inputEl[0].value = localSaveEl.email;
+  inputEl[1].value = localSaveEl.message;
+}
 
 inputEl.addEventListener('submit', onSubmitForm);
 
-function onSubmitForm (event) {
-    event.preventDefault();
-    localStorage.clear();
-    console.log(localEl);
-    inputEl.reset();
+function onSubmitForm(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.clear();
+  inputEl.reset();
 }
